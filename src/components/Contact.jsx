c328e1689a99a746381fc6cf24cc7be05977d44d
Gabriel Weidlich <143716541+GabrielWeidlich@users.noreply.github.com>
2025-04-10 16:06:07 -0300

    Fixes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -18,21 +18,21 @@ export default (props) => {
                 <div className="contact-info">
 
                     <div className="phone">
-                        <FontAwesomeIcon icon={faPhone} style={{ color: '#000000' }} size="xl" /><span>(62)9 8482-4811</span>
+                        <FontAwesomeIcon icon={faPhone} style={{ color: '#8AF2E9' }} size="xl" /><span>(62) 9 8482-4811</span>
                     </div>
                     <div className="email">
-                        <FontAwesomeIcon icon={faEnvelope} style={{ color: '#000000' }} size="xl" /><span>gabrielweidlichanaps@gmail.com</span>
+                        <FontAwesomeIcon icon={faEnvelope} style={{ color: '#9FCAD9' }} size="xl" /><span>gabrielweidlichanaps@gmail.com</span>
                     </div>
                     <div className="location">
-                        <FontAwesomeIcon icon={faLocationDot} style={{ color: '#000000' }} size="xl" /><span>Anápolis - GO</span>
+                        <FontAwesomeIcon icon={faLocationDot} style={{ color: '#9FACD9' }} size="xl" /><span>Anápolis - GO</span>
                     </div>
 
                 </div>
 
                 <div className="contact-sub-info">
-                    <a href="https://www.instagram.com/gabriel_dlich/" target="blank"><FontAwesomeIcon icon={faInstagram} size="lg" style={{color: '#f2f2f2'}}/></a>
-                    <a href="https://github.com/GabrielWeidlich" target="blank"><FontAwesomeIcon icon={faGithubSquare} size="lg" style={{color: '#f2f2f2'}}/></a>
-                    <a href="https://www.linkedin.com/in/gabriel-weidlich-/" target="blank"><FontAwesomeIcon icon={faLinkedin} size="lg" style={{color: '#f2f2f2'}}/></a>
+                    <a href="https://www.instagram.com/gabriel_dlich/" target="blank"><FontAwesomeIcon icon={faInstagram} size="lg" style={{color: '#8AF2E9'}}/></a>
+                    <a href="https://github.com/GabrielWeidlich" target="blank"><FontAwesomeIcon icon={faGithubSquare} size="lg" style={{color: '#9FCAD9'}}/></a>
+                    <a href="https://www.linkedin.com/in/gabriel-weidlich-/" target="blank"><FontAwesomeIcon icon={faLinkedin} size="lg" style={{color: '#9FACD9'}}/></a>
                 </div>
 
             </div>
